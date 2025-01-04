211879c710559f52034e3de9eb4c8b6619f72a63
Yurii Kramar <y.kramar15@gmail.com>
2025-01-04 13:46:56 +0100

    test package json

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import preact from '@preact/preset-vite'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [preact()],
-    base: 'https://github.com/yuraKrampus2021/todo-react-ts',
+    base: '/todo-react-ts/',
 })
