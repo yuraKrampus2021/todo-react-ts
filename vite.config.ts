37ae114a547b513bf6c21bcd9414d8fb53108caa
Yurii Kramar <y.kramar15@gmail.com>
2025-01-04 14:11:32 +0100

    fixing problem with deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import preact from '@preact/preset-vite'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [preact()],
-    base: '/todo-react-ts/',
+    base: '/todo-react-ts',
 })
